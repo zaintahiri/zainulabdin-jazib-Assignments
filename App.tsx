4a351e1795978e73d0f890d6ea194db322dd4ab8
bal-admin <zainulabdin.uos@gmail.com>
2024-03-10 14:13:03 +0500

    Assingment 2.2 completed by zain ul abdin and jazib sohail

--- App.tsx
+++ App.tsx
@@ -7,9 +7,10 @@
 
 // import React from 'react';
 
-import React, { useState } from 'react';
+import React, { useState , createContext, useContext} from 'react';
 import type {PropsWithChildren} from 'react';
 import ExStyle from './assets/images/Css/ExStyle';
+
 import {
   Alert,
   Button,
@@ -34,8 +35,10 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { blue } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
 
 const Stack = createNativeStackNavigator();
+
 
 function ProfileScreen() {
   return (
@@ -98,6 +101,172 @@ function SettingsScreen() {
     </View>
   );
 }
+
+
+// assignment 2.2
+
+function UsersListScreen() {
+  const [userList,setUserList] = useState([]);
+      async function fetchData() {
+        try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json();
+          setUserList(data)
+          //console.log('Data:', data);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+    }
+
+    
+  return (
+    <View style={[styles.container,
+      {
+        // Try setting `flexDirection` to `"row"`.
+        flexDirection: 'column',
+      }]}>
+      <View style={{flexDirection:'row',backgroundColor:'black'}}>
+          <Text style={{flex:1,textAlign:'center',padding:5,fontSize:20,color:'white'}}>
+                Users List
+          </Text>
+      </View>
+
+        <View style={{flexDirection:'row',marginTop:10,alignContent:'center',alignItems:'center'}}>
+          <View style={{flex:1}}/>
+            <View style={{flex:1}}>
+                <Button title='Show Data' color='green' onPress={()=>{
+                    fetchData();
+                }}/>
+
+            </View>
+            <View style={{flex:1}}/>
+        </View>  
+
+
+        <View style={{flexDirection:'row',marginTop:10,alignContent:'center',alignItems:'center'}}>
+
+        {     
+          userList!=null ? 
+              (
+
+                <View style={{flex:1,flexDirection:'column',marginTop:10,alignContent:'center',alignItems:'center'}}>
+
+                      
+                      <View style={{flexDirection:'row',marginStart:10,marginEnd:10}}>
+
+                      <ScrollView style={{flex:1}}>
+                          
+
+                              {
+                                    userList.map(item => {
+                                      return(
+                                        <View style={{flex:1,flexDirection:'column',margin:8,}}>
+                                        <View style={{flexDirection:'row',height:60,marginBottom:-8}}>
+                                          {/* <Text style={{flex:1,fontSize:15,padding:10,
+                                                          textAlign:'center',
+                                                          backgroundColor:'green',color:'white',marginTop:5,marginEnd:1}}>
+                                                {'Title'}
+                                          </Text> */}
+                                          <Text style={{flex:1,fontSize:15,padding:10,borderWidth:1,borderColor:'gray',borderRadius: 10,
+                                                          textAlign:'center',
+                                                          backgroundColor:'red',color:'white',}}>
+                                                {/* {'Description'} */}
+                                                
+                                                {item.title}
+                                          </Text>
+                                        </View>
+                                        <View style={{flexDirection:'row',height:150,backgroundColor:'white'}}>
+                                            {/* <Text style={{flex:1,fontSize:15,padding:10,
+                                                              backgroundColor:'red',color:'white',
+                                                              marginBottom:5,marginEnd:1}}>
+                                                    {item.title}
+                                              </Text> */}
+                                              <Text style={{flex:1,fontSize:15,padding:10,
+                                                              backgroundColor:'black',color:'white',
+                                                             }}>
+                                                    {item.body}
+                                              </Text>
+                                            
+                                      </View>   
+                                      </View>                                                                          
+                                      
+                                      )                               
+                                      
+                                    })
+                              }                              
+                             
+                              </ScrollView>
+                      </View>
+                     
+
+                </View>
+                
+                // <ScrollView>
+                //     <View style={{flexDirection:'column',margin:20,borderWidth:2,borderColor:'gray'}}>
+                //     <View style={{flexDirection:'row'}}>
+                //         <Text style={{flex:1,fontSize:15,padding:10,
+                //                         backgroundColor:'red',color:'white',marginTop:5,
+                //                         marginBottom:5}}>
+                //               {'Title'}
+                //         </Text>
+                //         <Text style={{flex:1,fontSize:15,padding:10,
+                //                         backgroundColor:'red',color:'white',marginTop:5,
+                //                         marginBottom:5}}>
+                //               {'Description'}
+                //         </Text>
+                //       </View>
+                //           {
+                //             userList.map(item => {
+                //               return(  
+                                
+                //                 <View style={{padding:5,flexDirection:'column'}}>
+                                 
+                //                   <View style={{flexDirection:'row'}}>
+                //                   <Text style={{flex:1,fontSize:15,padding:10,
+                //                                   backgroundColor:'red',color:'white',marginTop:5,
+                //                                   marginBottom:5}}>
+                //                         {item.title}
+                //                   </Text>
+                //                   <Text style={{flex:1,fontSize:15,padding:10,
+                //                                   backgroundColor:'red',color:'white',marginTop:5,
+                //                                   marginBottom:5}}>
+                //                         {item.body}
+                //                   </Text>
+                //                   </View>
+                                  
+                                  
+                //                 </View>
+          
+                                      
+                                
+                //               )
+                              
+                //             })
+                //           }
+                //    </View>
+                // </ScrollView>
+                      
+
+                
+                
+                  
+              )
+
+          : null
+                     
+        }
+
+        </View>
+          
+  </View>
+
+    
+  );
+}
+
+
+
+
 function HomeScreen() {
   const navigation = useNavigation();
   return (
@@ -120,6 +289,15 @@ function HomeScreen() {
                 <Button color="red" title='Settings' onPress={()=>{
                   // Alert.alert('Settings is called')
                   navigation.navigate("Settings"); //navigate("ProfileScreen");
+                  //adsfasfsafsaddfsfasdfsd
+                  //asdfasdfasdfasdf dsfasdfsafasd
+                }}/>
+            </View>
+
+            <View style={{flex:1,marginEnd:5}}>
+                <Button color="red" title='User List' onPress={()=>{
+                  // Alert.alert('Settings is called')
+                  navigation.navigate("UsersList"); //navigate("ProfileScreen");
                   //adsfasfsafsaddfsfasdfsd
                   //asdfasdfasdfasdf dsfasdfsafasd
                 }}/>
@@ -149,6 +327,14 @@ function HomeScreen() {
 }
 
 
+
+
+
+
+
+
+
+
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -165,6 +351,7 @@ function App(): React.JSX.Element {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="UsersList" component={UsersListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
 
@@ -282,6 +469,13 @@ const AssignmentTwo = () => {
   );
 };
 const styles = StyleSheet.create({
+ 
+  item: {
+    padding: 20,
+    fontSize: 15,
+    marginTop: 5,
+  },
+ 
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
